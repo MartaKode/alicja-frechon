033ef3c6d7da6d96d432a696a7decddf66246c74
61848346 <martaalicja113@gmail.com>
2020-12-23 06:04:47 -0500

    setting up about

--- src/components/About.js
+++ src/components/About.js
@@ -1,28 +1,7 @@
 import React, { useState } from 'react'
 
 const About = () => {
-    const [expanded, setExpanded] = useState(false)
-    const [accordionExpanded, setAccordionExpanded] = useState(false)
-    const [anchorEl, setAnchorEl] = React.useState(null)
 
-    const open = Boolean(anchorEl)
-
-    const handleExpandClick = () => {
-        setExpanded(!expanded)
-    }
-
-    const handlePopoverOpen = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-
-    const handlePopoverClose = () => {
-        setAnchorEl(null);
-    };
-
-    const handleAboutChange = () => {
-        setAccordionExpanded(!accordionExpanded)
-    }
  
     return (
         <div id='about'>
@@ -30,7 +9,6 @@ const About = () => {
         </div>
     )
 }
-
 
 
 export default About
