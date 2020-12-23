5d5a36b636026fe1f3abbb6c9b20b435fd213b50
61848346 <martaalicja113@gmail.com>
2020-12-23 05:53:21 -0500

    setting up about helper functions

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const About = () => {
+    const [expanded, setExpanded] = useState(false)
+    const [accordionExpanded, setAccordionExpanded] = useState(false)
+    const [anchorEl, setAnchorEl] = React.useState(null)
 
+    const open = Boolean(anchorEl)
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded)
+    }
+
+    const handlePopoverOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+
+    const handlePopoverClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleAboutChange = () => {
+        setAccordionExpanded(!accordionExpanded)
+    }
  
     return (
         <div id='about'>
