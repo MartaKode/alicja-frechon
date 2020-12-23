4e01a2764698b5c9cc2f05c2655e3d1594704ef5
61848346 <martaalicja113@gmail.com>
2020-12-23 05:37:26 -0500

    re-adding about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const About = () => {
 
-
+ 
     return (
         <div id='about'>
-            About Section
+    
         </div>
     )
 }
