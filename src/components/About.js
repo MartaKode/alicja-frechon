971ac21d6b843424ce480574bcbec0edbca1a966
61848346 <martaalicja113@gmail.com>
2020-12-23 05:25:19 -0500

    useStyles deployment test

--- src/components/About.js
+++ src/components/About.js
@@ -7,29 +7,6 @@ const useStyles = makeStyles((theme) => ({
 
 const About = () => {
 
-    const [expanded, setExpanded] = useState(false)
-    const [accordionExpanded, setAccordionExpanded] = useState(false)
-    const [anchorEl, setAnchorEl] = React.useState(null)
-
-    const classes = useStyles()
-    const open = Boolean(anchorEl)
-
-    const handleExpandClick = () => {
-        setExpanded(!expanded)
-    }
-
-    const handlePopoverOpen = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-
-    const handlePopoverClose = () => {
-        setAnchorEl(null);
-    };
-
-    const handleAboutChange = () => {
-        setAccordionExpanded(!accordionExpanded)
-    }
 
     return (
         <div id='about'>
