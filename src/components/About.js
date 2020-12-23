ea53528c90e42e6f8adfca266dfc029711493d28
61848346 <martaalicja113@gmail.com>
2020-12-23 05:22:09 -0500

    .

--- src/components/About.js
+++ src/components/About.js
@@ -2,55 +2,8 @@ import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        // maxWidth: '100%',
-        width: '100%',
-        display: 'flex',
-        // backgroundImage: `url(${beach})`,
-        // backgroundAttachment: 'fixed',
-        // backgroundRepeat: 'no-repeat',
-        // backgroundSize: 'cover',
-        boxShadow:'1px 1px 15px 0px black'
-    },
-    media: {
-        height: '63vh',
-        width: '40%',
-        // paddingTop: '56.25%', // 16:9
-    },
-    expand: {
-        transform: 'rotate(0deg)',
-        marginLeft: 'auto',
-        transition: theme.transitions.create('transform', {
-            duration: theme.transitions.duration.shortest,
-        }),
-    },
-    expandOpen: {
-        transform: 'rotate(180deg)',
-    },
-    popover: {
-        pointerEvents: 'none',
-    },
-    paper: {
-        padding: theme.spacing(1),
-    },
-    heading: {
-        fontSize: theme.typography.pxToRem(35),
-        flexBasis: '33.33%',
-        flexShrink: 0,
-    },
-    secondaryHeading: {
-        fontSize: theme.typography.pxToRem(15),
-        color: theme.palette.text.secondary,
-        flexBasis: '100%',
-        alignSelf: 'center',
-    },
-    aboutAccordion:{
-        // backgroundImage: `url(${background})`,
-        backgroundAttachment: 'fixed',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-    }
-}));
+    
+}))
 
 const About = () => {
 
