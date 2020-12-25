532fe34d8f81345f87dbbe7f443786a2643a0052
61848346 <martaalicja113@gmail.com>
2020-12-25 17:49:00 -0500

    about header added

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,11 +1,30 @@
+import { Accordion, AccordionSummary, Typography } from '@material-ui/core';
 import React, {useState} from 'react';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 
 const AboutPage = () => {
 const [expanded, setExpanded] = useState(false)
+const [accordionExpanded, setAccordionExpanded] = useState(false)
+
+const handleAboutChange = () => {
+    setAccordionExpanded(!accordionExpanded)
+}
 
     return(
         <div id='about'>
             <button onClick={() => setExpanded(!expanded)}>{expanded ? 'About' : 'Page' }</button>
+            <Accordion onChange={() => handleAboutChange()} >
+            <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <Typography >My Story</Typography>
+                    <Typography >{accordionExpanded ? 'Click to hide my story' : 'Click to see more about me'}</Typography>
+
+                </AccordionSummary>
+            </Accordion>
         </div>
     )
 }
