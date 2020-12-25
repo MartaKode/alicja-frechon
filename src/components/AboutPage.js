0901fe2a15d1a9353d1f2007fbdfa501a443bf41
61848346 <martaalicja113@gmail.com>
2020-12-25 17:43:07 -0500

    reversing about changes for deployemnt

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,29 +1,7 @@
 import React, {useState} from 'react';
-import { Accordion, AccordionDetails, AccordionSummary, Card, CardActions, CardContent, CardHeader, Collapse, IconButton, Popover, Typography } from '@material-ui/core';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import avatar from '../images/coverPhoto.jpg'
-import { Image } from 'antd';
 
 const AboutPage = () => {
 const [expanded, setExpanded] = useState(false)
-const [accordionExpanded, setAccordionExpanded] = useState(false)
-const [anchorEl, setAnchorEl] = useState(null)
-
-const handleExpandClick = () => {
-    setExpanded(!expanded)
-}
-
-const handlePopoverOpen = (event) => {
-    setAnchorEl(event.currentTarget)
-}
-
-const handlePopoverClose = () => {
-    setAnchorEl(null)
-}
-
-const handleAboutChange = () => {
-    setAccordionExpanded(!accordionExpanded)
-}
 
     return(
         <div id='about'>
@@ -33,4 +11,3 @@ const handleAboutChange = () => {
 }
 
 export default AboutPage
-
