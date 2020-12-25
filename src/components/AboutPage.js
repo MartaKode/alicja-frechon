47c491c8f60f798449be2b7945425678dee3e075
61848346 <martaalicja113@gmail.com>
2020-12-25 17:27:17 -0500

    about commit

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,7 +1,14 @@
 import React, {useState} from 'react';
+import { Accordion, AccordionDetails, AccordionSummary, Card, CardActions, CardContent, CardHeader, Collapse, IconButton, Popover, Typography } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import avatar from '../images/coverPhoto.jpg'
+import { Image } from 'antd';
 
 const AboutPage = () => {
 const [expanded, setExpanded] = useState(false)
+const [accordionExpanded, setAccordionExpanded] = useState(false)
+const [anchorEl, setAnchorEl] = useState(null)
+
 
     return(
         <div id='about'>
@@ -11,3 +18,4 @@ const [expanded, setExpanded] = useState(false)
 }
 
 export default AboutPage
+
