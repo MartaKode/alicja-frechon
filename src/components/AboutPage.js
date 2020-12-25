e18e8480a7b964159ef0a1bb2be2550b34bbf981
61848346 <martaalicja113@gmail.com>
2020-12-25 18:48:53 -0500

    expand button in About

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,24 +1,38 @@
-import { Accordion, AccordionDetails, AccordionSummary, Card, CardContent, CardHeader, Typography } from '@material-ui/core';
-import React, {useState} from 'react';
+import { Accordion, AccordionDetails, AccordionSummary, Card, CardActions, CardContent, CardHeader, IconButton, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { Image } from 'antd';
 import avatar from '../images/coverPhoto.jpg'
 
 
-
 const AboutPage = () => {
-const [expanded, setExpanded] = useState(false)
-const [accordionExpanded, setAccordionExpanded] = useState(false)
+    const [expanded, setExpanded] = useState(false)
+    const [accordionExpanded, setAccordionExpanded] = useState(false)
+    const [anchorEl, setAnchorEl] = useState(null)
 
-const handleAboutChange = () => {
-    setAccordionExpanded(!accordionExpanded)
-}
+    const open = Boolean(anchorEl)
 
-    return(
+    const handleExpandClick = () => {
+        setExpanded(!expanded)
+    }
+
+    const handlePopoverOpen = event => {
+        setAnchorEl(event.currentTarget)
+    }
+
+    const handlePopoverClose = () => {
+        setAnchorEl(null)
+    }
+
+    const handleAboutChange = () => {
+        setAccordionExpanded(!accordionExpanded)
+    }
+
+    return (
         <div id='about'>
-            <button onClick={() => setExpanded(!expanded)}>{expanded ? 'About' : 'Page' }</button>
+            <button onClick={() => setExpanded(!expanded)}>{expanded ? 'About' : 'Page'}</button>
             <Accordion onChange={() => handleAboutChange()} >
-            <AccordionSummary
+                <AccordionSummary
                     expandIcon={<ExpandMoreIcon />}
                     aria-controls="panel1a-content"
                     id="panel1a-header"
@@ -28,23 +42,41 @@ const handleAboutChange = () => {
 
                 </AccordionSummary>
                 <AccordionDetails>
-                <Card>
-                <div className='cardInitial'>
-                    <CardHeader 
-                    title='Personal'
-                    subheader='Trainer'
-                    />
+                    <Card>
+                        <div className='cardInitial'>
+                            <CardHeader
+                                title='Personal'
+                                subheader='Trainer'
+                            />
 
-                    <Image src={avatar} />
-                    
-                    <CardContent>
-                    <Typography variant="body2" color="textSecondary" component="p">
+                            <Image src={avatar} />
+
+                            <CardContent>
+                                <Typography variant="body2" color="textSecondary" component="p">
                                     This impressive paella is a perfect party dish and a fun meal to cook together with your
                                     guests. Add 1 cup of frozen peas along with the mussels, if you like.
                         </Typography>
-                    </CardContent>
-                </div>
-                </Card>
+                            </CardContent>
+                        </div>
+                        <CardActions disableSpacing>
+                            <IconButton
+                             className={'expand', {
+                                ['expandOpen']: expanded,
+                            }}
+                                lick={handleExpandClick}
+                                aria-expanded={expanded}
+                                aria-label="show more"
+                            >
+                                <ExpandMoreIcon style={{ transform: 'rotate(-90deg)' }}
+                                    aria-owns={open ? 'mouse-over-popover' : undefined}
+                                    aria-haspopup="true"
+                                    onMouseEnter={handlePopoverOpen}
+                                    onMouseLeave={handlePopoverClose}
+                                />
+
+                            </IconButton>
+                        </CardActions>
+                    </Card>
                 </AccordionDetails>
             </Accordion>
         </div>
